669830f19aa6989d739a3c9e3756446bd71284a1
fuyusakaiori <670232228@qq.com>
2023-03-08 13:49:55 +0800

    [feature]: 初始化网关模块

---
+++ neptune-im-gateway/src/main/java/com/fuyusakaiori/nep/im/gateway/NeptuneImGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fuyusakaiori.nep.im.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeptuneImGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeptuneImGatewayApplication.class, args);
+	}
+
+}
---
+++ neptune-im-gateway/src/test/java/com/fuyusakaiori/nep/im/gateway/NeptuneImGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuyusakaiori.nep.im.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeptuneImGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
